c4d9f325fe882ed429cb0d7c1fa264df7b5dd0d5
Daniil Kosachev <kos-dan@mail.ru>
2019-06-09 18:40:54 +0300

    Input file full path

--- Program.fs
+++ Program.fs
@@ -2,6 +2,7 @@
 
 [<EntryPoint>]
 let main argv =
-    let game = Game.readFromFileWithInt "grid"
+    let game = Game.readFromFileWithInt "D:\\projects\\F#\\GameOfLife\\grid"
     game.RunInConsoleWithDelay(250)
     0 // return an integer exit code
+
