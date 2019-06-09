4706113cf11a7415d46ddec0924bbaf29e4cc8e3
Daniil Kosachev <kos-dan@mail.ru>
2019-06-09 17:41:52 +0300

    Remade with members and module functions

--- Game.fs
+++ Game.fs
@@ -8,32 +8,52 @@ type private Change = {
     state: bool
 }
 
-let correctIndex index maxIndex =
+let private correctIndex index maxIndex =
     match index with
     | index when index >= maxIndex -> index % maxIndex
     | index when index < 0 -> index + maxIndex
     | _ -> index
 
-type Game(grid: bool[][]) =  // TODO: with bool[,]
-    let isLivingCell(i, j) =
-        let rowCount = grid.Length
-        let columnCount = grid.[0].Length
-        grid.[correctIndex i rowCount].[correctIndex j columnCount]
+let private doDelay (msDelay: int) =
+    System.Threading.Thread.Sleep(msDelay)
 
-    let countLivingNeigbours i j =
+let private clearScreen() =
+    System.Console.Clear()
+
+let private printGrid grid =
+    let livengCellMark = "o"
+    let deadCellMark = " "
+    let boolArrayToMarksString array =
+        array
+        |> Array.map (fun x -> if x then livengCellMark else deadCellMark)
+        |> String.concat ""
+
+    let stringGrid =
+        grid
+        |> Array.map boolArrayToMarksString
+        |> String.concat "\n"
+
+    printfn "%s" stringGrid
+
+// TODO: with bool[,]
+type Game = { Grid: bool[][] } with
+    member this.isLivingCell(i, j) =
+        this.Grid.[correctIndex i this.RowCount].[correctIndex j this.ColumnCount]
+
+    member this.countLivingNeigbours i j =
         let isNeighbour iAdd jAdd = iAdd <> jAdd || iAdd <> 0
         // TODO: remade this block
         let shifts = [ -1..1 ]
         let mutable count = 0
         for iShift in shifts do
             for jShift in shifts do
-                if isNeighbour iShift jShift && isLivingCell(i + iShift, j + jShift) then
+                if isNeighbour iShift jShift && this.isLivingCell(i + iShift, j + jShift) then
                     count <- count + 1
         count
 
-    let processCell i j =
-        let livingCount = countLivingNeigbours i j
-        if isLivingCell(i, j) then
+    member private this.processCell i j =
+        let livingCount = this.countLivingNeigbours i j
+        if this.isLivingCell(i, j) then
             if livingCount < 2 || livingCount > 3 then
                 Some { i = i;  j = j; state = false}
             else
@@ -43,53 +63,35 @@ type Game(grid: bool[][]) =  // TODO: with bool[,]
                 Some { i = i;  j = j; state = true}
             else None
 
-    let doStep() =
+    member this.doStep() =
         let processRow index row =
             row
-            |> Array.mapi (fun i _ -> processCell index i)
-        grid
+            |> Array.mapi (fun i _ -> this.processCell index i)
+        this.Grid
         |> Array.mapi processRow
         |> Array.concat
         |> Array.choose id
-        |> Array.iter (fun x -> grid.[x.i].[x.j] <- x.state)
+        |> Array.iter (fun x -> this.Grid.[x.i].[x.j] <- x.state)
 
-    let doDelay (msDelay: int) =
-        System.Threading.Thread.Sleep(msDelay)
+    member this.RowCount = this.Grid.Length
 
-    let clearScreen() =
-        System.Console.Clear()
-
-    let printGrid() =
-        let livengCellMark = "o"
-        let deadCellMark = " "
-        let boolArrayToMarksString array =
-            array
-            |> Array.map (fun x -> if x then livengCellMark else deadCellMark)
-            |> String.concat ""
-
-        let stringGrid =
-            grid
-            |> Array.map boolArrayToMarksString
-            |> String.concat "\n"
-
-        printfn "%s" stringGrid
+    member this.ColumnCount = this.Grid.[0].Length
 
     member public this.RunInConsoleWithDelay msDelay =
         for count in Seq.initInfinite((+)1) do
             clearScreen()
             printfn "Step %d:" count
-            printGrid()
-            doStep()
-            doDelay(msDelay)
+            printGrid this.Grid
+            this.doStep()
+            doDelay msDelay
 
 let public readFromFileWithInt filename =
     let toBoolArray charArray =
         charArray
         |> Array.map ((=)'1')
 
-
     let boolMatrix =
         File.ReadAllLines filename
         |> Array.map (Seq.toArray >> toBoolArray)
 
-    Game boolMatrix
+    { Grid=boolMatrix }
